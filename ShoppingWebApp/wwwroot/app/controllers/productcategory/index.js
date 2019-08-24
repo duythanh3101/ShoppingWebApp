b3acadbf2a3c4b41ab1e5a4616014caba417fdc1
duythanh3101 <1612623@student.hcmus.edu.vn>
2019-08-24 20:44:56 +0700

    create product category in admin area

--- ShoppingWebApp/wwwroot/app/controllers/productcategory/index.js
+++ ShoppingWebApp/wwwroot/app/controllers/productcategory/index.js
@@ -1,7 +1,120 @@
 ﻿var categoryController = function () {
     this.initialize = function () {
         loadData();
+        registerEvents();
     };
+
+    function registerEvents() {
+        $('#btnCreate').on('click', function () {
+            initTreeDropDownCategory();
+            $('#modalAddEdit').modal('show');
+        });
+
+        $('#btnSave').on('click', function (e) {
+            if ($('#frmMaintainance').valid()) {
+                e.preventDefault();
+                var id = parseInt($('#hidIdM').val());
+                var name = $('#txtNameM').val();
+                var parentId = $('#ddlCategoryIdM').combotree('getValue');
+                var description = $('#txtDescM').val();
+
+                var image = $('#txtImageM').val();
+                var order = parseInt($('#txtOrderM').val());
+                var homeOrder = $('#txtHomeOrderM').val();
+
+                var seoKeyword = $('#txtSeoKeywordM').val();
+                var seoMetaDescription = $('#txtSeoDescriptionM').val();
+                var seoPageTitle = $('#txtSeoPageTitleM').val();
+                var seoAlias = $('#txtSeoAliasM').val();
+                var status = $('#ckStatusM').prop('checked') == true ? 1 : 0;
+                var showHome = $('#ckShowHomeM').prop('checked');
+
+                $.ajax({
+                    type: "POST",
+                    url: "/Admin/ProductCategory/SaveEntity",
+                    data: {
+                        Id: id,
+                        Name: name,
+                        Description: description,
+                        ParentId: parentId,
+                        HomeOrder: homeOrder,
+                        SortOrder: order,
+                        HomeFlag: showHome,
+                        Image: image,
+                        Status: status,
+                        SeoPageTitle: seoPageTitle,
+                        SeoAlias: seoAlias,
+                        SeoKeywords: seoKeyword,
+                        SeoDescription: seoMetaDescription
+                    },
+                    dataType: "json",
+                    beforeSend: function () {
+                        utilities.startLoading();
+                    },
+                    success: function (response) {
+                        utilities.notify('Update success', 'success');
+                        $('#modalAddEdit').modal('hide');
+
+                        resetFormMaintainance();
+
+                        utilities.stopLoading();
+                        loadData(true);
+                    },
+                    error: function () {
+                        utilities.notify('Has an error in update progress', 'error');
+                        utilities.stopLoading();
+                    }
+                });
+            }
+            return false;
+
+        });
+    }
+    function resetFormMaintainance() {
+        $('#hidIdM').val(0);
+        $('#txtNameM').val('');
+        initTreeDropDownCategory('');
+
+        $('#txtDescM').val('');
+        $('#txtOrderM').val('');
+        $('#txtHomeOrderM').val('');
+        $('#txtImageM').val('');
+
+        $('#txtMetakeywordM').val('');
+        $('#txtMetaDescriptionM').val('');
+        $('#txtSeoPageTitleM').val('');
+        $('#txtSeoAliasM').val('');
+
+        $('#ckStatusM').prop('checked', true);
+        $('#ckShowHomeM').prop('checked', false);
+    }
+
+    function initTreeDropDownCategory(selectedId) {
+        $.ajax({
+            url: "/Admin/ProductCategory/GetAll",
+            type: 'GET',
+            dataType: 'json',
+            async: false,
+            success: function (response) {
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder
+                    });
+                });
+                var arr = utilities.unflattern(data);
+                $('#ddlCategoryIdM').combotree({
+                    data: arr
+                });
+                if (selectedId !== undefined) {
+                    $('#ddlCategoryIdM').combotree('setValue', selectedId);
+                }
+            }
+        });
+    }
 
     var loadData = function () {
         $.ajax({
@@ -25,6 +138,16 @@
                 $('#categoryTree').tree({
                     data: treeArr,
                     dnd: true,
+                    onContextMenu: function (e, node) {
+                        e.preventDefault();
+                        // select the node
+                        //$('#tt').tree('select', node.target);
+                        // display context menu
+                        $('#contextMenu').menu('show', {
+                            left: e.pageX,
+                            top: e.pageY
+                        });
+                    },
                     onDrop: function (target, source, point) {
                         var targetNode = $(this).tree('getNode', target);
                         if (point === 'append') {
